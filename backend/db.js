622485bf563068710294eff1fe20a803fc21b7e9
dhanushkumar <dhanushkumar558@gmail.com>
2025-08-04 16:12:24 +0530

    Done

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'dhanush',
-  host: 'localhost',
-  database: 'inventory',
-  password: 'root123!',
-  port: 5432,
+  user: 'avnadmin',
+  host: 'pg-e2eef21-dhanushkumar558-b8b4.i.aivencloud.com',
+  database: 'defaultdb',
+  password: 'AVNS_KxCtcF3dEko0QoZZQcv',
+  port: 11283,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
