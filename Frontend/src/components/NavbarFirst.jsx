9a3753808903655ad3ea3f0e342f519e45363b26
dhanushkumar <dhanushkumar558@gmail.com>
2025-08-08 16:19:16 +0530

    Done

--- Frontend/src/components/NavbarFirst.jsx
+++ Frontend/src/components/NavbarFirst.jsx
@@ -3,11 +3,12 @@ export default function NavbarFirst(){
     return (
       <nav className="bg-white shadow-md fixed top-0 left-0 right-0 z-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="h-16 flex items-center justify-center">
-            <span className="text-2xl font-bold text-blue-600 tracking-wide">
-              Stocify
-            </span>
-          </div>
+        <div className="h-16 flex items-center justify-start">
+  <span className="text-2xl font-bold text-blue-600 tracking-wide">
+    Stocify
+  </span>
+</div>
+
         </div>
       </nav>
     );
