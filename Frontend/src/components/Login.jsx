a37308a01170c7d3278f8f06b6b755e07af12c13
dhanushkumar <dhanushkumar558@gmail.com>
2025-08-08 14:19:07 +0530

    Done

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import API from '../api';
 import { toast } from 'react-hot-toast';
+import Navbar from './Navbar';
 
 export default function Login({ onLogin }) {
   const [username, setUsername] = useState('');
@@ -29,6 +30,8 @@ export default function Login({ onLogin }) {
   };
 
   return (
+    <>
+    <Navbar />
     <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
       <div className="flex flex-col md:flex-row items-start md:items-center gap-8 max-w-4xl w-full justify-center">
         
@@ -89,5 +92,5 @@ export default function Login({ onLogin }) {
 
       </div>
     </div>
-  );
+  </>);
 }
